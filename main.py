d9331e23f814c7ec93cd87988f77f1aa9b711d88
shnathan <83175979+shnathan@users.noreply.github.com>
2022-08-09 22:57:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,108 +1,15 @@
-import mediapipe as mp
 import cv2
 from cvzone.HandTrackingModule import HandDetector
 from time import sleep
 import numpy as np
 import cvzone
 from pynput.keyboard import Controller
-class handDetector():
-    def __init__(self, mode=False, maxHands=2, detectionCon=0.5,modelComplexity=1,trackCon=0.5):
-        self.mode = mode
-        self.maxHands = maxHands
-        self.detectionCon = detectionCon
-        self.modelComplex = modelComplexity
-        self.trackCon = trackCon
-        self.mpHands = mp.solutions.hands
-        self.hands = self.mpHands.Hands(self.mode, self.maxHands,self.modelComplex,
-                                    self.detectionCon, self.trackCon)
-        self.mpDraw = mp.solutions.drawing_utils # it gives small dots onhands total 20 landmark points
 
-    def findHands(self,img,draw=True):
-    # Send rgb image to hands
-        imgRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-        self.results = self.hands.process(imgRGB) # process the frame
-#     print(results.multi_hand_landmarks)
-
-        if self.results.multi_hand_landmarks:
-            for handLms in self.results.multi_hand_landmarks:
-
-                if draw:
-                    #Draw dots and connect them
-                    self.mpDraw.draw_landmarks(img,handLms,
-                                            self.mpHands.HAND_CONNECTIONS)
-
-        return img
-
-    def findPosition(self, img, handNo=0, draw=True):
-        xList = []
-        yList = []
-        bbox = []
-        self.lmList = []
-        if self.results.multi_hand_landmarks:
-            myHand = self.results.multi_hand_landmarks[handNo]
-            for id, lm in enumerate(myHand.landmark):
-                # print(id, lm)
-                h, w, c = img.shape
-                cx, cy = int(lm.x * w), int(lm.y * h)
-                xList.append(cx)
-                yList.append(cy)
-                # print(id, cx, cy)
-                self.lmList.append([id, cx, cy])
-                if draw:
-                    cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
-
-            xmin, xmax = min(xList), max(xList)
-            ymin, ymax = min(yList), max(yList)
-            bbox = xmin, ymin, xmax, ymax
-
-            if draw:
-                cv2.rectangle(img, (xmin - 20, ymin - 20), (xmax + 20, ymax + 20),
-                              (0, 255, 0), 2)
-
-        return self.lmList, bbox
-#
-#     def fingersUp(self):
-#         fingers = []
-#         # Thumb
-#         if self.lmList[self.tipIds[0][1]] > self.lmList[self.tipIds[0] - 1][1]:
-#             fingers.append(1)
-#         else:
-#             fingers.append(0)
-#
-#         # Fingers
-#         for id in range(1, 5):
-#
-#             if self.lmList[self.tipIds[id][2]] < [self.lmList[self.tipIds[id] - 2][2]]:
-#                 fingers.append(1)
-#             else:
-#                 fingers.append(0)
-#
-#         # totalFingers = fingers.count(1)
-#
-#         return fingers
-#
-#     def findDistance(self, p1, p2, img, draw=True, r=15, t=3):
-#         x1, y1 = self.lmList[p1][1:]
-#         x2, y2 = self.lmList[p2][1:]
-#         cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
-#
-#         if draw:
-#             cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), t)
-#             cv2.circle(img, (x1, y1), r, (255, 0, 255), cv2.FILLED)
-#             cv2.circle(img, (x2, y2), r, (255, 0, 255), cv2.FILLED)
-#             cv2.circle(img, (cx, cy), r, (0, 0, 255), cv2.FILLED)
-#         length = math.hypot(x2 - x1, y2 - y1)
-#
-#         return length, img, [x1, y1, x2, y2, cx, cy]
-#import cv2 as cv
-#from cvzone.HandTrackingModule import HandDetector
-#from module1 import findposition
 cap = cv2.VideoCapture(0)
 cap.set(3, 1280)
 cap.set(4, 720)
 
-detector = handDetector()
-#detector = self.mpHands.Hands(self.mode, self.maxHands, self.detectionCon, self.trackCon)
+detector = HandDetector(detectionCon=0.8)
 keys = [["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
         ["A", "S", "D", "F", "G", "H", "J", "K", "L", ";"],
         ["Z", "X", "C", "V", "B", "N", "M", ",", ".", "/"]]
@@ -115,14 +22,11 @@ def drawAll(img, buttonList):
     for button in buttonList:
         x, y = button.pos
         w, h = button.size
-        #cvzone.cornerRect(img, (button.pos[0], button.pos[1], button.size[0], button.size[1]),
-                          #20, rt=0)
+        cvzone.cornerRect(img, (button.pos[0], button.pos[1], button.size[0], button.size[1]),
+                          20, rt=0)
         cv2.rectangle(img, button.pos, (x + w, y + h), (255, 0, 255), cv2.FILLED)
         cv2.putText(img, button.text, (x + 20, y + 65),
                     cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 4)
-
-
-
     return img
 
 
@@ -137,12 +41,12 @@ def drawAll(img, buttonList):
 #                       (255, 0, 255), cv2.FILLED)
 #         cv2.putText(imgNew, button.text, (x + 40, y + 60),
 #                     cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 3)
-# #
-# #     out = img.copy()
-# #     alpha = 0.5
-# #     mask = imgNew.astype(bool)
-# #     print(mask.shape)
-# #     out[mask] = cv2.addWeighted(img, alpha, imgNew, 1 - alpha, 0)[mask]
+#
+#     out = img.copy()
+#     alpha = 0.5
+#     mask = imgNew.astype(bool)
+#     print(mask.shape)
+#     out[mask] = cv2.addWeighted(img, alpha, imgNew, 1 - alpha, 0)[mask]
 #     return out
 
 
@@ -163,34 +67,31 @@ while True:
     img = detector.findHands(img)
     lmList, bboxInfo = detector.findPosition(img)
     img = drawAll(img, buttonList)
-    # for x,key in enumerate(keys[0]):
-    #     buttonList.append(Button([100*x+50,100],key))
 
     if lmList:
         for button in buttonList:
             x, y = button.pos
             w, h = button.size
 
-            if x < lmList[8][0] < x + w :
-                cv2.rectangle(img, (x +w, y +h), (0, 255, 0), cv2.FILLED)
+            if x < lmList[8][0] < x + w and y < lmList[8][1] < y + h:
+                cv2.rectangle(img, (x - 5, y - 5), (x + w + 5, y + h + 5), (175, 0, 175), cv2.FILLED)
                 cv2.putText(img, button.text, (x + 20, y + 65),
                             cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 4)
                 l, _, _ = detector.findDistance(8, 12, img, draw=False)
                 print(l)
 
-                # when clicked
-                if l<30:
+                ## when clicked
+                if l < 30:
                     keyboard.press(button.text)
-                    cv2.rectangle(img,  (x-5 , y -5),(x+w+5,y+h+5), (0, 255, 0), cv2.FILLED)
+                    cv2.rectangle(img, button.pos, (x + w, y + h), (0, 255, 0), cv2.FILLED)
                     cv2.putText(img, button.text, (x + 20, y + 65),
                                 cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 4)
                     finalText += button.text
                     sleep(0.15)
-                    print(finalText)
 
-    cv2.rectangle(img, (50, 350), (700, 450), (175, 0, 175), cv2.FILLED)
-    cv2.putText(img, finalText, (60, 430),
-                cv2.FONT_HERSHEY_PLAIN, 5, (255, 255, 255), 5)
-    #img=drawAll(img,buttonList)
+    #cv2.rectangle(img, (50, 350), (700, 450), (175, 0, 175), cv2.FILLED)
+    # cv2.putText(img, finalText, (60, 430),
+    #             cv2.FONT_HERSHEY_PLAIN, 5, (255, 255, 255), 5)
+
     cv2.imshow("Image", img)
     cv2.waitKey(1)
